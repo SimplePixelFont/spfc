5cf77e711e40580a23e080a70bd7d96f9d2d10b5
The-Nice-One <nicedude618@gmail.com>
2026-02-16 22:08:51 -0500

    Fixed timestamp issue in Head table https://github.com/fonttools/fontbakery/issues/1763

--- src/builders/head.rs
+++ src/builders/head.rs
@@ -1,11 +1,23 @@
 use super::Process;
 use anyhow::Result;
+use std::time::{SystemTime, UNIX_EPOCH};
 use write_fonts::{
     tables::head::{Head, MacStyle},
-    types::Fixed,
+    types::{Fixed, LongDateTime},
 };
 
+fn unix_to_mac_epoch(unix_time: u64) -> i64 {
+    const MAC_EPOCH_OFFSET: u64 = 2082844800;
+    (unix_time + MAC_EPOCH_OFFSET) as i64
+}
+
 pub fn push_head_table(process: &mut Process) -> Result<()> {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let mac_timestamp = unix_to_mac_epoch(now);
+
     let head = Head {
         units_per_em: process.units_per_em,
         x_min: 0,
@@ -14,6 +26,8 @@ pub fn push_head_table(process: &mut Process) -> Result<()> {
         y_max: process.units_per_em as i16,
         mac_style: MacStyle::empty(),
         font_revision: Fixed::from_f64(process.family_version),
+        created: LongDateTime::new(mac_timestamp),
+        modified: LongDateTime::new(mac_timestamp),
         ..Default::default()
     };
     process.builder.add_table(&head)?;
