d5e6a7a1ef0815df7be59302865bc4ae9a6e0115
The-Nice-One <nicedude618@gmail.com>
2026-04-02 13:11:17 -0400

    add missing compiler symbols in build_tarballs.jl

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -68,4 +68,4 @@ end
 dependencies = Dependency[
 ]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"10.2.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", compilers=[:rust, :c])
